62e134fd3dbde7b0643714adeb5ff9b5f6d47d91
khaykay <berryscott97@gmail.com>
2023-03-19 23:44:02 +0100

    action comments

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      react and typescript to do app with local storage, tailwind css
+    </div>
+  );
 }
 
 export default App;
