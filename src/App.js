b95d8bb617fefb6ac4530d8085371a35d22a6b2d
khaykay <berryscott97@gmail.com>
2023-03-21 23:30:42 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      react and typescript to do app with local storage, tailwind css
+      react and typescript to do app with local storage, tailwind css last day
+      of joking around
     </div>
   );
 }
